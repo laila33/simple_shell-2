da9b713a905083a2af4c69ba2fffbc8344ecee5b
lila abdelnaser <98240363+laila33@users.noreply.github.com>
2023-06-30 01:45:56 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -62,7 +62,7 @@ alias_t *aliases;
 /* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char **_strtok(char *line, char *delim);
+char **strtok_func(char *x, char *d);
 char *get_location(char *command);
 list_t *get_path_dir(char *path);
 int execute(char **args, char **front);
